935a1aa471e0481747c709a9acd116e16bbb0228
Eric Youngberg <eric@lmtlss.net>
2020-01-16 12:06:18 -0500

    Setup an RGB spectrum viewer

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
-import { Window } from 'genera';
+/**
+ * Color - Spectrums
+ */
 
 
+import PixelSpace from 'genera/src/PixelSpace';
+import ColorSpectrum from 'genera/src/light/ColorSpectrum';
+
+const workspace = document.querySelector("#workspace");
+const canvas = new PixelSpace('#workspace', workspace.clientWidth, workspace.clientHeight);
+const spectrum = new ColorSpectrum("RGGBRB");
+const period = Math.floor(spectrum.length / canvas.width);
+
+for (let col = 0; col < canvas.width; col++) {
+    canvas.setColumn(col, spectrum.color(period * col));
+}
