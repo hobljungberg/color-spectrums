93a826e53829bf6947f91eb598d25a5e3593f65b
Eric Youngberg <eric@lmtlss.net>
2020-02-21 09:57:59 -0500

    Test combinatons in Collections

--- src/index.js
+++ src/index.js
@@ -5,12 +5,18 @@
 
 import PixelSpace from 'genera/src/PixelSpace';
 import ColorSpectrum from 'genera/src/light/ColorSpectrum';
+import Collection from 'genera/src/combine/Collection';
 
 const workspace = document.querySelector("#workspace");
 const canvas = new PixelSpace('#workspace', workspace.clientWidth, workspace.clientHeight);
 const spectrum = new ColorSpectrum("RGGBRB");
 const period = Math.floor(spectrum.length / canvas.width);
 
-for (let col = 0; col < canvas.width; col++) {
-    canvas.setColumn(col, spectrum.color(period * col));
-}
+//const spectrums = new Collection(['r', 'g', 'b', 'r', 'g', 'b']).combinations;
+const spectrums = new Collection(['r', 'r', 'g', 'g', 'b', 'b']).combinations().map(combo => combo.reduce((a,b) => a + b));
+
+console.log(spectrums);
+
+//for (let col = 0; col < canvas.width; col++) {
+    //canvas.setColumn(col, spectrum.color(period * col));
+//}
